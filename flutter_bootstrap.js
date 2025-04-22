e4a0ca95e11e1f5889b628a432c9401c659d4ba7
duckhiem2003 <duckhiem2003@gmail.com>
2025-04-22 13:05:54 +0700

    Change the path

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2508376379"
+    serviceWorkerVersion: "1704922510"
   }
 });
