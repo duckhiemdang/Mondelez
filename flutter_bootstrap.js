3245244bba692a9cb5e9492ab847cad0b22d4d8a
duckhiem2003 <duckhiem2003@gmail.com>
2025-05-03 00:27:46 +0700

    Update version without login stuff

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4029740436"
+    serviceWorkerVersion: "3938749462"
   }
 });
